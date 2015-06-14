6d31a2502b6dfdf5d873c421fdf39872fc856f8c
magicode <magicode7@gmail.com>
2015-06-14 21:40:57 +0300

    add overflow image

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ module.exports = addon;
 var fs = require('fs');
 
 module.exports.addImageTitle(fs.readFileSync(__dirname + '/checking.png'), 1);
+module.exports.addImageTitle(fs.readFileSync(__dirname + '/overflow.png'), 2);
